6f5e2c62f17fab3a93077b5e259c85beafc4ff07
Joshua Munene <kenyadevelop@gmail.com>
2015-06-19 18:28:01 -0700

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   def home
     if current_user
       redirect_to t_shirts_path
     end
+  end
+
+  def dashboard
+    @t_shirts = current_user.t_shirts
   end
 end
